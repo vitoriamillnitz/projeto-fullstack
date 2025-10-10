8b20a91cd80ded202fad2defb46b1ec968a95b29
vitoriamillnitz <vitoriamillnitz@gmail.com>
2025-10-09 23:19:40 -0300

    Corrige todos os pontos do feedback e reestrutura o projeto

--- src/App.js
+++ src/App.js
@@ -1,86 +1,79 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import { ToastContainer } from 'react-toastify';
 
+import SeasonProvider from './context/SeasonContext';
+import LoginProvider from './context/LoginContext';
 import DriverProvider from './context/DriverContext';
-import SeasonProvider from "./context/SeasonContext";
-import TeamProvider from "./context/TeamContext";
-import RaceProvider from "./context/RaceContext";
+import TeamProvider from './context/TeamContext';
+import RaceProvider from './context/RaceContext';
 
-import Menu from "./components/menu/Menu";
+import Menu from './components/menu/Header'; 
 import Driver from './components/driver/Driver';
-import Table from "./components/table/Table";
-import Races from "./components/races/Races";
-import Home from "./components/home/Home";
-import Login from "./components/login/Login";
-import LoginProvider from "./context/LoginContext";
-import Private from "./components/private/Private";
-import TableEdit from "./components/tableEdit/TableEdit";
-import EditProvider from "./context/EditContext";
-import FormCreateDriver from "./components/formCreateDriver/FormCreateDriver";
-import FormCreateTeam from "./components/formCreateTeam/FormCreateTeam";
-import FormCreateRace from "./components/formCreateRace/FormCreateRace";
+import Table from './components/table/Table';
+import FormCreateDriver from './components/formCreateDriver/FormCreateDriver';
+import FormCreateTeam from './components/formCreateTeam/FormCreateTeam';
+import FormCreateRace from './components/formCreateRace/FormCreateRace';
+
+import Home from './pages/home/Home';
+import Login from './pages/login/Login';
+import Races from './pages/races/Races';
+import Private from './pages/private/Private';
 
 function App() {
   return (
     <BrowserRouter basename="/projeto-fullstack">
-    <div className="screen">
-      <SeasonProvider>
-        <LoginProvider >
-        <Menu />
-        <main className="info">
-          <Routes>
-            <Route path="/Projeto-Formula-1" element={
-              <Home></Home>
-            } />
-            <Route path="/login" element={
-              <Login></Login>
-            } />
-            <Route path="/pilots" element={
-              <DriverProvider>  
-                <Driver></Driver>
-                <Table type={"drivers"}></Table>
-              </DriverProvider>
-            } />
-            <Route path="/teams" element={
-              <TeamProvider>
-                <Table type={"teams"}></Table>
-              </TeamProvider>
-            } />
-            <Route path="/races" element={
-              <RaceProvider>
-                <Races></Races>
-              </RaceProvider>
-            } />
-            <Route path="/editpilots" element={
-              <Private>
-                <EditProvider type={"pilotos"}>
-                  <TableEdit type={"drivers"}></TableEdit>
-                </EditProvider>
-              </Private>
-            } />
-            <Route path="/editteams" element={
-              <Private>
-                <EditProvider type={"equipes"}>
-                  <TableEdit type={"drivers"}></TableEdit>
-                </EditProvider>
-              </Private>
-            } />
-            <Route path="/create" element={
-              <Private>
-                <div className="createForms">
-                <FormCreateDriver></FormCreateDriver>
-                <FormCreateTeam></FormCreateTeam>
-                <FormCreateRace></FormCreateRace>
-                </div>
-              </Private>
-            } />
-          </Routes>
-        </main>
-        </LoginProvider>
-      </SeasonProvider>
-    </div>
-    <ToastContainer />
+      <div className="screen">
+        <SeasonProvider>
+          <LoginProvider>
+            <Menu />
+            <main className="info">
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/login" element={<Login />} />
+                <Route
+                  path="/pilots"
+                  element={
+                    <DriverProvider>
+                      <Driver />
+                      <Table type={"drivers"} />
+                    </DriverProvider>
+                  }
+                />
+                <Route
+                  path="/teams"
+                  element={
+                    <TeamProvider>
+                      <Table type={"teams"} />
+                    </TeamProvider>
+                  }
+                />
+                <Route
+                  path="/races"
+                  element={
+                    <RaceProvider>
+                      <Races />
+                    </RaceProvider>
+                  }
+                />
+                <Route
+                  path="/create"
+                  element={
+                    <Private>
+                      <div className="createForms">
+                        <FormCreateDriver />
+                        <FormCreateTeam />
+                        <FormCreateRace />
+                      </div>
+                    </Private>
+                  }
+                />
+              </Routes>
+            </main>
+          </LoginProvider>
+        </SeasonProvider>
+      </div>
+      <ToastContainer />
     </BrowserRouter>
   );
 }
